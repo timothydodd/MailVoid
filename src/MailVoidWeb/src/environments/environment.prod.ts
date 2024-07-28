424acd59e247d53492902fa706994cde895cf90c
Timothy Dodd <tdodd@viewfi.com>
2024-07-28 15:07:05 -0400

    updated app url

--- src/MailVoidWeb/src/environments/environment.prod.ts
+++ src/MailVoidWeb/src/environments/environment.prod.ts
@@ -7,7 +7,7 @@ export const environment = {
   auth0: {
     domain: 'dev-fgsthtfl2egy63uu.us.auth0.com',
     client_id: '09VPqGTDbVX3Nsgwpf07j8EQ99AHD6kq',
-    redirect_uri: 'https://void.dbmk2.com/authorize',
+    redirect_uri: 'https://app.dbmk2.com/authorize',
     audience: 'https://void.dbmk2.com',
   },
 };
