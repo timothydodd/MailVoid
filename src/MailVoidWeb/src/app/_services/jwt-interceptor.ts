28c096ce3172c398487beece01df1c0084cdff73
Timothy Dodd <tdodd@viewfi.com>
2024-10-11 12:25:35 -0400

    updated latest packages

--- src/MailVoidWeb/src/app/_services/jwt-interceptor.ts
+++ src/MailVoidWeb/src/app/_services/jwt-interceptor.ts
@@ -1,12 +1,11 @@
 import { HTTP_INTERCEPTORS, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { AuthService } from './auth-service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
   constructor(private authService: AuthService) {}
-
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = this.authService.getToken();
 
@@ -18,7 +17,33 @@ export class JwtInterceptor implements HttpInterceptor {
       });
     }
 
-    return next.handle(request);
+    return next.handle(request).pipe(
+      catchError((err) => {
+        if (err.status === 401) {
+          // auto logout if 401 response returned from api
+          this.authService.logout();
+        }
+
+        let error = '';
+        if (err) {
+          if (err.error) {
+            const val = err.error as ValidationError;
+
+            if (val) {
+              return throwError(val);
+            }
+          }
+          if (err.message) {
+            error = err.message;
+          } else if (err.error && err.error.message) {
+            error = err.error.message || err.statusText;
+          }
+        }
+        return throwError(() => {
+          return error;
+        });
+      })
+    );
   }
 }
 
@@ -27,3 +52,11 @@ export const JwtInterceptorProvider = {
   useClass: JwtInterceptor,
   multi: true,
 };
+export interface ValidationError {
+  message: string;
+  errors: FieldError[];
+}
+export interface FieldError {
+  message: string;
+  field: string;
+}
